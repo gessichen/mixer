82abec16dfd6735a06c8745023f0e375ab67b8e9
CHEN GUO <chenguo@Gessi.local>
2018-10-06 09:30:37 -0700

    mixer first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express =  require("express");
+var http = require('http');
+var path = require("path");
+
+var app = express();
+app.use(express.static(path.join(__dirname, "Public")));
+
+var server = app.listen(3000);
+console.log('Server listening on port 3000');
+  
+
+
